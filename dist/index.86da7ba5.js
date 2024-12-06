aa3e0c82a7d0d1c84423de11aabc08f95fe1caf5
ToeCrow <thomas.kronvold@chasacademy.se>
2024-12-06 14:47:45 +0100

    erj

--- dist/index.86da7ba5.js
+++ dist/index.86da7ba5.js
@@ -1,3 +1,45 @@
+//? Mockdata
+const cityWeather = [
+    {
+        name: "G\xf6teborg",
+        weatherDescription: "Sol",
+        temperatur: 18
+    },
+    {
+        name: "Malm\xf6",
+        weatherDescription: "Molning",
+        temperatur: 15
+    },
+    {
+        name: "Ume\xe5",
+        weatherDescription: "Klart",
+        temperatur: 10
+    }
+];
+//? mockdata slut
+function displayCard() {
+    const weatherSection = document.getElementById("weather-cards");
+    if (!weatherSection) {
+        console.error("Elementet med id 'weather-cards' hittades inte.");
+        return;
+    }
+    cityWeather.forEach((city)=>{
+        const card = document.createElement("article");
+        const cityName = document.createElement("h2");
+        const weather = document.createElement("p");
+        const temperature = document.createElement("p");
+        card.classList.add("card");
+        cityName.classList.add("city");
+        cityName.textContent = city.name;
+        weather.classList.add("weather");
+        weather.textContent = city.weatherDescription;
+        temperature.classList.add("temperature");
+        temperature.textContent = `${city.temperatur}\xb0C`;
+        card.append(cityName, weather, temperature);
+        weatherSection.append(card);
+    });
+}
+displayCard();
 function saveToLocalStorage(key, data) {
     try {
         localStorage.setItem(key, JSON.stringify(data));
@@ -6,4 +48,4 @@ function saveToLocalStorage(key, data) {
     }
 }
 
-//# sourceMappingURL=index.86da7ba5.js.map
+
