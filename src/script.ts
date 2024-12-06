d871726289d563123162ab519aef4cbcfd8597ea
Gustav Thilander <gustav.thilander@gmail.com>
2024-12-06 15:32:13 +0100

    Update script.ts

--- src/script.ts
+++ src/script.ts
@@ -1,17 +1,3 @@
-type City = {
-    name: string;
-    lat: number;
-    long: number;
-  };
-  
-const cities = [
-    { name: "Göteborg", lat: 57.7089, long: 11.9746 },
-    { name: "Malmö", lat: 55.6050, long: 13.0038 },
-    { name: "Linköping", lat: 58.4109, long: 15.6216 },
-    { name: "Skellefteå", lat: 64.7500, long: 20.9500 },
-    { name: "Umeå", lat: 63.8258, long: 20.2630 },
-  ];
-  console.log(cities);
 
 type Card = { //! Card interface type, temporary
     city: string,
