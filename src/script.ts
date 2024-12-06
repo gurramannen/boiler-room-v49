544b1baff669e65daa426334db0edde3dc6b7d47
Gustav Thilander <gustav.thilander@gmail.com>
2024-12-06 14:46:48 +0100

    fixat drop down filter funktion

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,17 @@
+type City = {
+    name: string;
+    lat: number;
+    long: number;
+  };
+  
+const cities = [
+    { name: "Göteborg", lat: 57.7089, long: 11.9746 },
+    { name: "Malmö", lat: 55.6050, long: 13.0038 },
+    { name: "Linköping", lat: 58.4109, long: 15.6216 },
+    { name: "Skellefteå", lat: 64.7500, long: 20.9500 },
+    { name: "Umeå", lat: 63.8258, long: 20.2630 },
+  ];
+  console.log(cities);
 
 type Card = { //! Card interface type, temporary
     city: string,
@@ -34,3 +48,22 @@ function getFromLocalStorage<T>(key: string): T | null {
 
 const retrievedWeatherCards = getFromLocalStorage<Card[]>('weatherCards');
 console.log(retrievedWeatherCards); // This will log your array of cards
+
+const citySelect = document.getElementById("city-select") as HTMLSelectElement;
+
+// Kontrollera att elementet faktiskt hittas
+if (!citySelect) {
+  throw new Error("Elementet med id 'city-select' hittades inte.");
+}
+
+citySelect.addEventListener("change", () => {
+    const selectedCityName = citySelect.value;
+    const selectedCity = cities.find((city) => city.name === selectedCityName);
+  
+    if (selectedCity) {
+      fetchWeather(selectedCity.lat, selectedCity.long);
+    }
+  });
+
+const initialCity = cities[0];
+fetchWeather(initialCity.lat, initialCity.long);
